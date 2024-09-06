9eceb10d12e5d23071c70dfa9bf248649464f291
thiagograssopuc <thiago.grasso@pucpr.edu.br>
2024-09-06 11:56:21 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        function validarFormulario() {
+            var email = document.getElementById("email").value;
+            var senha = document.getElementById("senha").value;
+            if (email === "" || senha === "") {
+                alert("Por favor, preencha todos os campos.");
+                return false;
+            } else {
+                window.location.href = 'entrar.html';
+                return true;
+            }
+        }
+    </script>
+</head>
+<body>
+    <div>
+        <h2> Bem-vindo(a)!</h2>
+    </div>
+    <div>
+        <label>E-mail:</label> <input type="email" id="email" placeholder="E-mail"> <br>
+        <label>Senha: </label> <input type="password" id="senha" placeholder="Senha"> <br>
+        <button onclick="return validarFormulario()">Entrar</button>
+    </div>
+    <div>
+        <h3>Não possui conta?</h3>
+        <h3><a href="cadastrar.html">Cadastre-se!</a></h3>
+    </div>
+</body>
+</html>
+
+    
